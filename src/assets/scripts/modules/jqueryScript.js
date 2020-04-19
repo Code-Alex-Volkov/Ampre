5b31e6b1f335428a641bd4d58863aef995a118ab
Alex Volkov <alexvolkov72305@yandex.by>
2020-04-19 08:26:49 +0300

    08:26 19.04.2020

--- src/assets/scripts/modules/jqueryScript.js
+++ src/assets/scripts/modules/jqueryScript.js
@@ -1,38 +1,30 @@
-// Подключаем jquery (https://github.com/webpack/webpack/issues/4258)
 var $ = require("jquery"); 
 
 $(document).ready(function(){
 	'use strict';
 
-	/* other */
+	/* btn menu */
 	$('.nav_btn_mobile').click(function(){
 	   $('.nav_btn_mobile').toggleClass('nav-active');
 		$('.nav_mobile').toggleClass('nav-active_mobile');
 		$('.check_in').toggleClass('check-active');
 	});
 
-if ($('.line_content_mobile').length) {
-			if($('body').innerWidth() < 480){
-		     $('.line_content_mobile').slick({
-			  centerMode: true,
-			  centerPadding: '15px',
-			  slidesToShow: 1,
-			  arrows: false
+
+	/* slider choose */
+	if ($('.line_content_mobile').length) {
+		if($('body').innerWidth() < 480){
+			$('.line_content_mobile').slick({
+				centerMode: true,
+				centerPadding: '15px',
+				slidesToShow: 1,
+				arrows: false
 			});
 			$('.line_content').addClass('line_content_mobile');
-
-	    }
-	    else{
-	     
+		}
+		else{
 			$('.line_content').revomeClass('line_content_mobile');
-	    }
-		
+		}		
 	}
-
-	
-
-
-
-
 
 });
